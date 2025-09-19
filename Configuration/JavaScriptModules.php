388f2ac24b2e9b7045827ed516c5e29ce7935c61
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2025-09-19 22:11:44 +0200

    fix: Don't recursively import scripts from EXT:backend
    This is unneeded and breaks overrides from other extensions

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -11,9 +11,6 @@ return [
         'backend.module',
     ],
     'imports' => [
-        // recursive definition, all *.js files in this folder are import-mapped
-        // trailing slash is required per importmap-specification
-        '@typo3/backend/' => 'EXT:backend/Resources/Public/JavaScript/',
         '@WebVision/WvFileCleanup/' => 'EXT:wv_file_cleanup/Resources/Public/JavaScript/',
     ]
 ];
